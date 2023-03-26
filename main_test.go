354a3e66c7ebdbdd102343fcf600bf292fa4c573
Jason McNeil <sixcolors@mac.com>
2023-03-26 10:42:57 -0300

    fix test error message

--- main_test.go
+++ main_test.go
@@ -10,6 +10,6 @@ func TestAccepts(t *testing.T) {
 
 	// assert that mimetype is application/json
 	if mimetype != "application/json" {
-		t.Errorf("mimetype is %s, want application/octet-stream", mimetype)
+		t.Errorf("mimetype is %s, want application/json", mimetype)
 	}
 }
